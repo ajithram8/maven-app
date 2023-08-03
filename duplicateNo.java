ba95ba78eb3461dba53d0229d76206059f1996b4
ajithram8 <131467910+ajithram8@users.noreply.github.com>
2023-08-03 22:45:48 +0530

    Add files via upload

---
+++ duplicateNo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class duplicateNo {
+public static void main(String []args){
+	ArrayList a=new ArrayList();
+	a.add("a");
+		for(int b:a){
+			System.out.println(b);
+		}
+
+}
+}
+  
